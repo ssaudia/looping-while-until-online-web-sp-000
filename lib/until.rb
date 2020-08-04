0b48787016e7ae27c78d0655d3e036d6075fc98b
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-04 03:57:06 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,7 @@
 def using_until
-  levitation_force = 6
-  #your code here
-    
+loop do
+  puts "Wingardium Leviosa"
 end
-
+  levitation_force = 7
+  #your code here
+end
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,8 @@
 def using_while
-  levitation_force = 6
-	
+  loop do
+  puts "Wingardium Leviosa"
+  break
+  end
+  levitation_force = 7
   #your code here
 end
-
-
